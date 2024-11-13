4494a40565d361feddf2520ffe81abac8d485c29
tgbrooks <tgbrooks@gmail.com>
2024-11-13 14:15:35 -0500

    Support multiple fastq files
    Removes corresponding reads from all if any are found to match ribo

--- ribo_remover.py
+++ ribo_remover.py
@@ -1,18 +1,22 @@
 import argparse
 
 parser = argparse.ArgumentParser(
-    description = "Ribo Remover\nUsing NCBI blastn to compare fastq files to a ribo database and remove any that match"
+    description = "Ribo Remover\nUses NCBI blastn to compare fastq files to a ribo database of mammalian ribo and remove any that match"
 )
-parser.add_argument("--input_fastq", help="filename of fastq to filter. If ends in .gz, then assumed to be gzipped.")
-parser.add_argument("--output_fastq", help="filename to write filtered fastq to. If ends in .gz, will output as gzipped")
+parser.add_argument("--input_fastqs", help="filenames of fastq files to filter. If ends in .gz, then assumed to be gzipped. If more than one fastq, all are assumed to have the same reads (such as for paired ends) in the same order and are filtered out if any read matches the database", nargs="+")
+parser.add_argument("--output_fastqs", help="filenames to write filtered fastq to. If ends in .gz, will output as gzipped. One filename per input fastq", nargs="+")
 parser.add_argument("--ribo_db", help="blast db of ribo to filter against", default="blast_db/ribodb")
-parser.add_argument("--num_threads", help="number of threads to use for blast", default=1, type=int)
+parser.add_argument("--num_threads", help="number of threads to use per blastn instance (one for each input fastq)", default=1, type=int)
 
 args = parser.parse_args()
+
+assert len(args.input_fastqs) == len(args.output_fastqs), "Must have same number of input fastqs as output fastqs"
+
 
 import gzip
 import subprocess
 import time
+import contextlib
 
 RIBO_DB = args.ribo_db
 
@@ -32,59 +36,76 @@ def maybe_gzip_open(filename, *args):
     else:
         return open(filename, *args)
 
-CAT = "zcat" if args.input_fastq.endswith(".gz") else "cat"
-cmd = f"{CAT} {args.input_fastq} | " \
-    "sed -n '1~4s/^@/>/p;2~4p' | " \
-    f"{BLAST_DIR}/bin/blastn -task blastn -db {RIBO_DB} -query - -outfmt '10 qseqid' -evalue {E_VALUE_THRESHOLD} -num_threads {args.num_threads}"
-print(cmd)
+# Start blastn on each input fastq
+blast_procs = []
+for input_fastq in args.input_fastqs:
+    CAT = "zcat" if input_fastq.endswith(".gz") else "cat"
+    cmd = f"{CAT} {input_fastq} | " \
+        "sed -n '1~4s/^@/>/p;2~4p' | " \
+        f"{BLAST_DIR}/bin/blastn -task blastn -db {RIBO_DB} -query - -outfmt '10 qseqid' -evalue {E_VALUE_THRESHOLD} -num_threads {args.num_threads}"
+    print(cmd)
 
-blast = subprocess.Popen(
-    cmd,
-    shell=True,
-    stdout = subprocess.PIPE,
-    stderr = subprocess.PIPE,
-)
+    blast = subprocess.Popen(
+        cmd,
+        shell=True,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    blast_procs.append(blast)
 
 start_time = time.time()
 num_filtered = 0
 num_unfiltered = 0
-filtered_id = None
+filtered_ids: list[None | str] = [None for _ in blast_procs]
 DONE = "!!!DONE!!!"
-with maybe_gzip_open(args.input_fastq, "rb") as in_fastq, maybe_gzip_open(args.output_fastq, "wb") as out_fastq:
+with contextlib.ExitStack() as stack:
+    # Open input fastqs
+    in_fastqs = [stack.enter_context(maybe_gzip_open(input_fastq, "rb")) for input_fastq in args.input_fastqs]
+    out_fastqs = [stack.enter_context(maybe_gzip_open(output_fastq, "wb")) for output_fastq in args.output_fastqs]
+
     while True:
-        header_line: str = in_fastq.readline()
-        if not header_line:
-            # We're done reading the file
+        # Read one read from each input fastq
+        header_lines = [in_fastq.readline() for in_fastq in in_fastqs]
+        if all(not header for header in header_lines):
+            # We're done reading the files
             break
-        seq_line = in_fastq.readline()
-        separator_line = in_fastq.readline()
-        quality_line = in_fastq.readline()
+        seq_lines = [in_fastq.readline() for in_fastq in in_fastqs]
+        separator_lines = [in_fastq.readline() for in_fastq in in_fastqs]
+        quality_lines = [in_fastq.readline() for in_fastq in in_fastqs]
 
-        id = header_line.removeprefix(b"@").split(b" ")[0]
+        # Extract read id
+        ids = [header_line.removeprefix(b"@").split(b" ")[0]
+                for header_line in header_lines]
+        assert len(set(ids)) == 1, f"FASTQ files did not have matching read ids at: {', '.join(header_lines)}"
+        id = ids[0]
 
-        filtered = filtered_id == id
-        while filtered_id != DONE:
-            blastout = blast.stdout.readline()
-            if not blastout:
-                # We have finished with all blast output, everything else is NOT filtered
-                filtered_id = DONE
-                break
-            filtered_id = blastout.strip()
-            if filtered_id == id:
-                filtered = True
-            else:
-                # Any ID other than our current one indicates BLAST is ahead of our position
-                # and so we need to stop looking to see if this is filtered
-                break
+        # Advance to next output of each blastn process
+        filtered = any(filtered_id == id for filtered_id in filtered_ids)
+        for idx, blast in enumerate(blast_procs):
+            while filtered_ids[idx] != DONE:
+                blastout = blast.stdout.readline()
+                if not blastout:
+                    # We have finished with all blast output, everything else is NOT filtered
+                    filtered_ids[idx] = DONE
+                    break
+                filtered_ids[idx] = blastout.strip()
+                if filtered_ids[idx] == id:
+                    filtered = True
+                else:
+                    # Any ID other than our current one indicates BLAST is ahead of our position
+                    # and so we need to stop looking to see if this is filtered
+                    break
+
         if filtered:
             num_filtered += 1
-            continue
-
-        num_unfiltered += 1
-        out_fastq.write(header_line)
-        out_fastq.write(seq_line)
-        out_fastq.write(separator_line)
-        out_fastq.write(quality_line)
+        else:
+            num_unfiltered += 1
+            # Output to each fastq
+            for (header, seq, sep, qual, out) in zip(header_lines, seq_lines, separator_lines, quality_lines, out_fastqs):
+                out.write(header)
+                out.write(seq)
+                out.write(sep)
+                out.write(qual)
 
 end_time = time.time()
 print(f"Out of {num_filtered + num_unfiltered} total reads, {num_filtered} ribo found and {num_unfiltered} remain")
