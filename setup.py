725e12983e46ad7954aeb99b22c1dfc5d075dc2c
tgbrooks <tgbrooks@gmail.com>
2025-01-02 15:34:06 -0500

    Package ribo_remover

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ribo-remover",
+    version="0.1.0",
+    #packages=find_packages(where="src"),
+    packages = ["ribo_remover"],
+    package_dir={"": "src"},
+    package_data={
+        "ribo_remover": ["data/*"],
+    },
+    python_requires=">=3.10",
+)
