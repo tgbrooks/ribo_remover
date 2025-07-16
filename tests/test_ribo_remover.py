d941823807f52023019102d7afc8d9483bbf9900
Thomas Brooks <tgbrooks@gmail.com>
2025-07-16 11:22:51 -0400

    Add test of two fastqs

--- tests/test_ribo_remover.py
+++ tests/test_ribo_remover.py
@@ -3,7 +3,7 @@ import pathlib
 from ribo_remover.ribo_remover import ribo_remover
 
 
-def test_ribo_remover(tmp_path):
+def test_one_fastq(tmp_path):
     out_R1 = tmp_path / "out.R1.fastq"
     stats_file = tmp_path / "stats.txt"
     ribo_remover(
@@ -12,9 +12,31 @@ def test_ribo_remover(tmp_path):
         stats_file=stats_file,
     )
     out_contents = out_R1.read_text()
-    ref_contents = pathlib.Path("tests/resources/R1.out.R1.fastq").read_text()
+    ref_contents = pathlib.Path("tests/resources/one_fastq.out.R1.fastq").read_text()
     assert out_contents == ref_contents
 
     out_stat = stats_file.read_text()
-    ref_stat = pathlib.Path("tests/resources/stats.R1.txt").read_text()
+    ref_stat = pathlib.Path("tests/resources/one_fastq.stats.txt").read_text()
+    assert ref_stat == out_stat
+
+
+def test_two_fastqs(tmp_path):
+    out_R1 = tmp_path / "out.R1.fastq"
+    out_R2 = tmp_path / "out.R2.fastq"
+    stats_file = tmp_path / "stats.txt"
+    ribo_remover(
+        ["tests/resources/R1.fastq", "tests/resources/R2.fastq"],
+        [str(out_R1), str(out_R2)],
+        stats_file=stats_file,
+    )
+    out_contents = out_R1.read_text()
+    ref_contents = pathlib.Path("tests/resources/two_fastqs.out.R1.fastq").read_text()
+    assert out_contents == ref_contents
+
+    out_contents = out_R2.read_text()
+    ref_contents = pathlib.Path("tests/resources/two_fastqs.out.R2.fastq").read_text()
+    assert out_contents == ref_contents
+
+    out_stat = stats_file.read_text()
+    ref_stat = pathlib.Path("tests/resources/two_fastqs.stats.txt").read_text()
     assert ref_stat == out_stat
